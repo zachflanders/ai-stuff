de62db878f3208894ec67f3a67c734e82fc5dbbc
zachflanders <zachflanders@gmail.com>
2023-04-16 22:57:47 -0500

    adding visualizations

--- coursera-machine-learning/README.md
+++ coursera-machine-learning/README.md
@@ -21,8 +21,10 @@ $$\frac{\partial}{\partial \theta_j}J(\theta) = \text{The partial derivative of 
 ## Linear Regression
 ### Hypothesis Function
 $$Y  = h_{\theta}(X)= \theta_o + \theta_1X$$
+![Graph with scatter plot and linear regression](./ex1_linear_regression/prediction.png)
 ### Cost Function
 $$J(\theta_0, \theta_1) = \frac{1}{2n}\sum_{i=1}^n(\hat{Y}_i - Y_i)^2$$
+![Line chart of costs going down.](./ex1_linear_regression/costs_per_iteration.png)
 ### Gradient Descent
 $$ \text{repeat until convergence } \lbrace 
     \\\\
@@ -31,3 +33,4 @@ $$ \text{repeat until convergence } \lbrace
     \theta_1 := \theta_1 - \alpha \frac{1}{m}\sum_{i=1}^m(h_{\theta}(x_i) - y_i)x_i
     \\\\
 \rbrace$$
+![Graph with gradient descent surface](./ex1_linear_regression/gradient_descent.png)
--- coursera-machine-learning/ex1_linear_regression/ex1.py
+++ coursera-machine-learning/ex1_linear_regression/ex1.py
@@ -1,30 +1,75 @@
 
 from pathlib import Path
 
-import pandas
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt, cm
 
-def hypothesis(X, theta):
-    return theta[0] + X * theta[1]
+class LinearRegression:
 
-def cost(X, Y, theta):
-    return (1 / (2 * len(X))) * (hypothesis(X, theta) - Y).pow(2).sum()
+    def __init__(self, X, Y, initial_theta=(0, 0), alpha=0.01, iterations=1500) -> None:
+        self.X = X
+        self.Y = Y
+        self.theta = [initial_theta]
+        self.alpha = alpha
+        self.iterations = iterations
+        self.train()
 
-def gradient_descent(X, Y, theta, alpha):
-    return [
-        theta[0] - (alpha / len(X)) * (hypothesis(X, theta) - Y).sum(),
-        theta[1] - (alpha / len(X)) * ((hypothesis(X, theta) - Y) * X).sum(),
-    ]
+    def hypothesis(self, theta):
+        return theta[0] + self.X * theta[1]
 
-def predict(X, Y, theta, alpha, iterations):
-    for i in range(iterations):        
-        theta = gradient_descent(X, Y, theta, alpha)
-    return hypothesis(X, theta), theta
+    def cost(self, theta):
+        return (1 / (2 * len(self.X))) * (self.hypothesis(theta) - self.Y).pow(2).sum()
 
-def predict_one(population, theta):
-    return pandas.Series([1, population / 1000]).dot(pandas.Series(theta)) * 10000
+    def gradient_descent(self, theta):
+        return (
+            theta[0] - (self.alpha / len(self.X)) * (self.hypothesis(theta) - self.Y).sum(),
+            theta[1] - (self.alpha / len(self.X)) * ((self.hypothesis(theta) - self.Y) * self.X).sum(),
+        )
 
-data = pandas.read_csv(Path(__file__).parent / 'food_truck_data.csv')
-data['prediction'], final_theta = predict(X=data['pop'], Y=data['profit'], theta=[0, 0], alpha=0.01, iterations=1500)
-scatter_plot = data.plot.scatter(x='pop', y='profit', marker='x', color='red')
-data.plot.line(x='pop', y='prediction', ax=scatter_plot).get_figure().savefig('prediction.png')
-print(predict_one(35000, final_theta))
+    def train(self):
+        for i in range(self.iterations):        
+            self.theta.append(self.gradient_descent(self.theta[-1]))
+
+    def predict(self, x):
+        return pd.Series((1, x)).dot(pd.Series(self.theta[-1]))
+
+# Read data into pandas.DataFrame
+data = pd.read_csv(Path(__file__).parent / 'food_truck_data.csv')
+
+# Initialize linear regression
+lr = LinearRegression(X=data['pop'], Y=data['profit'])
+
+# Make predictions
+data['prediction'] = data['pop'].apply(lr.predict)
+
+# Visalize Predictions
+fig, ax = plt.subplots()
+ax.scatter(x=data['pop'], y=data['profit'], marker='x', color='red')
+ax.set_ylabel('Profit ($1000s)')
+ax.set_xlabel('City Population (1000s)')
+ax.set_title('Food Truck Profits')
+ax.plot(data['pop'], data['prediction'])
+fig.savefig(Path(__file__).parent / 'prediction.png')
+
+
+# Visalize Cost Curve
+costs_fig, costs_ax = plt.subplots()
+costs_ax.plot([lr.cost(theta) for theta in lr.theta])
+costs_ax.set_ylabel('Cost (Mean Sum of Squared Error Terms)')
+costs_ax.set_xlabel('Iteration')
+costs_ax.set_title('Costs Per Iteration')
+costs_fig.savefig(Path(__file__).parent / 'costs_per_iteration.png')
+
+# Visalize Gradient Descent
+grad_fig, grad_ax = plt.subplots(subplot_kw={'projection': '3d'})
+grad = pd.DataFrame({
+    'theta_0': np.random.uniform(-10, 10, size=1000), 
+    'theta_1': np.random.uniform(4, -4, size=1000),
+})
+grad['cost'] = grad.apply(lambda x: lr.cost([x['theta_0'], x['theta_1']]), axis=1)
+grad_ax.plot_trisurf(grad['theta_1'], grad['theta_0'], grad['cost'], cmap=cm.jet, linewidth=0)
+grad_ax.set_ylabel('theta_0')
+grad_ax.set_xlabel('theta_1')
+grad_ax.set_zlabel('Cost')
+grad_fig.savefig(Path(__file__).parent / 'gradient_descent.png')
