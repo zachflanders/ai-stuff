b0b5c329ab5a57305f6c84ad37ec1f38fdc1651b
Zach Flanders <zachflanders@gmail.com>
2023-04-24 13:59:40 -0500

    adding support for multivariate linear regression.

--- coursera-machine-learning/ex1_linear_regression/ex1.py
+++ coursera-machine-learning/ex1_linear_regression/ex1.py
@@ -5,43 +5,18 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt, cm
 
-class LinearRegression:
+from .linear_regression import LinearRegression
 
-    def __init__(self, X, Y, initial_theta=(0, 0), alpha=0.01, iterations=1500) -> None:
-        self.X = X
-        self.Y = Y
-        self.theta = [initial_theta]
-        self.alpha = alpha
-        self.iterations = iterations
-        self.train()
-
-    def hypothesis(self, theta):
-        return theta[0] + self.X * theta[1]
-
-    def cost(self, theta):
-        return (1 / (2 * len(self.X))) * (self.hypothesis(theta) - self.Y).pow(2).sum()
-
-    def gradient_descent(self, theta):
-        return (
-            theta[0] - (self.alpha / len(self.X)) * (self.hypothesis(theta) - self.Y).sum(),
-            theta[1] - (self.alpha / len(self.X)) * ((self.hypothesis(theta) - self.Y) * self.X).sum(),
-        )
-
-    def train(self):
-        for i in range(self.iterations):        
-            self.theta.append(self.gradient_descent(self.theta[-1]))
-
-    def predict(self, x):
-        return pd.Series((1, x)).dot(pd.Series(self.theta[-1]))
 
 # Read data into pandas.DataFrame
 data = pd.read_csv(Path(__file__).parent / 'food_truck_data.csv')
 
 # Initialize linear regression
-lr = LinearRegression(X=data['pop'], Y=data['profit'])
+lr = LinearRegression(X=data[['pop']], Y=data['profit'])
 
 # Make predictions
-data['prediction'] = data['pop'].apply(lr.predict)
+data['prediction'] = lr.predict()
+
 
 # Visalize Predictions
 fig, ax = plt.subplots()
@@ -61,15 +36,19 @@ costs_ax.set_xlabel('Iteration')
 costs_ax.set_title('Costs Per Iteration')
 costs_fig.savefig(Path(__file__).parent / 'costs_per_iteration.png')
 
-# Visalize Gradient Descent
-grad_fig, grad_ax = plt.subplots(subplot_kw={'projection': '3d'})
-grad = pd.DataFrame({
-    'theta_0': np.random.uniform(-10, 10, size=1000), 
-    'theta_1': np.random.uniform(4, -4, size=1000),
-})
-grad['cost'] = grad.apply(lambda x: lr.cost([x['theta_0'], x['theta_1']]), axis=1)
-grad_ax.plot_trisurf(grad['theta_1'], grad['theta_0'], grad['cost'], cmap=cm.jet, linewidth=0)
-grad_ax.set_ylabel('theta_0')
-grad_ax.set_xlabel('theta_1')
-grad_ax.set_zlabel('Cost')
-grad_fig.savefig(Path(__file__).parent / 'gradient_descent.png')
+# # Visalize Gradient Descent
+# grad_fig, grad_ax = plt.subplots(subplot_kw={'projection': '3d'})
+# grad = pd.DataFrame({
+#     'ones': np.random.uniform(-10, 10, size=1000), 
+#     'pop': np.random.uniform(4, -4, size=1000),
+# })
+# grad['cost'] = grad.apply(lambda x: lr.cost{ones: x['ones'], x['pop']]), axis=1)
+# grad_ax.plot_trisurf(grad['ones'], grad['pop'], grad['cost'], cmap=cm.jet, linewidth=0)
+# grad_ax.set_ylabel('ones')
+# grad_ax.set_xlabel('theta_1')
+# grad_ax.set_zlabel('Cost')
+# grad_fig.savefig(Path(__file__).parent / 'gradient_descent.png')
+
+housing_data = pd.read_csv(Path(__file__).parent /'house_values.csv')
+housing_lr = LinearRegression(X=housing_data[['sqft', 'bdrms']], Y=housing_data['value'])
+housing_data['prediction'] = housing_lr.predict()
---
+++ coursera-machine-learning/ex1_linear_regression/linear_regression.py
@@ -0,0 +1,42 @@
+from itertools import chain
+
+import pandas as pd
+
+class LinearRegression:
+
+    def __init__(self, X, Y, initial_theta=None, alpha=0.01, iterations=1500) -> None:
+        self.mus = X.mean()
+        self.stds = X.std()
+        self.X = self._normalize_features(X)
+        self.Y = Y
+        self.theta = [initial_theta]
+        if initial_theta is None:
+            self.theta = [{**{col: 0 for col in X.columns}, **{'ones': 0}}]
+        self.alpha = alpha
+        self.iterations = iterations
+        self.train()    
+    
+    def _normalize_features(self, x):
+        return (x - self.mus) / self.stds
+    def hypothesis(self, theta):
+        return theta['ones'] + sum([self.X[k] * v for k, v in theta.items() if k != 'ones'])
+
+    def cost(self, theta):
+        return (1 / (2 * len(self.X))) * (self.hypothesis(theta) - self.Y).pow(2).sum()
+
+    def gradient_descent(self, theta):
+        return {
+            **{'ones': theta['ones'] - (self.alpha / len(self.X)) * (self.hypothesis(theta) - self.Y).sum()},
+            **{
+                k: v - (self.alpha / len(self.X)) * ((self.hypothesis(theta) - self.Y) *(self.X[k])).sum()
+                for k, v in theta.items()
+                if k != 'ones'
+            }
+        }
+
+    def train(self):
+        for i in range(self.iterations):
+            self.theta.append(self.gradient_descent(self.theta[-1]))
+
+    def predict(self):
+        return self.X.assign(ones=1).apply(lambda x: x.dot(pd.Series(self.theta[-1])), axis=1)
